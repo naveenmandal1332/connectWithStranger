5649bf1d48e48a0010b07bd4725003190829ceae
naveenaegis <naveenaegis@gmail.com>
2023-09-17 23:04:49 +0530

    complete login

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const initialState = {
+    username: "",
+    gender: "",
+  }
+  const [formData, setFormData] = useState(initialState);
+
+  const handleInput = (e)=>{
+    const {name , value} = e.target;
+    setFormData({
+      ...formData,
+      [name]:value,
+    })
+  }
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    console.log("name: ", formData.username)
+    console.log("gender: ", formData.gender)
+
+    // Empty input field:
+    setFormData(initialState)
+  }
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -31,11 +54,12 @@ const Login = () => {
                   Your Name
                 </label>
                 <input
-                  type="name"
+                  type="username"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  name="name"
-                  id="name"
-                  placeholder="naveen"
+                  name="username"
+                  id="username"
+                  value={formData.username}
+                  onChange={handleInput}
                   required=""
                 />
               </div>
@@ -51,12 +75,14 @@ const Login = () => {
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   name="gender"
                   id="gender"
-                  placeholder="male"
+                  value={formData.gender}
+                  onChange={handleInput}
                   required=""
                 />
               </div>
               <button
                 type="submit"
+                onClick={onSubmit}
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
